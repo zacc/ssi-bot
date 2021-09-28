d45534c702eb5b4158b7f71207423fc5a925274b
Zac <zacc@users.noreply.github.com>
2021-09-28 15:06:20 +0800

    Hard code some negative keywords as a line of defence when the user d… (#34)
    Hard code some negative keywords as a line of defence when the user does not implement their own protections.

--- reddit_io.py
+++ reddit_io.py
@@ -43,8 +43,28 @@ class RedditIO(threading.Thread, LogicMixin):
 	_image_post_frequency = 0
 	_image_post_search_prefix = ''
 
+	# These hard-coded negative keywords are used to prevent the bot posting distasteful stuff.
+	# It's only inteded as a basic defence.
+	# Operator should train their model to avoid falling back on this.
+	_default_negative_keywords = [
+		('ar', 'yan'), ('ausch, witz'),
+		('black', ' people'),
+		('child p', 'orn'), ('concentrati', 'on camp'),
+		('fag', 'got'),
+		('hit', 'ler'), ('holo', 'caust'),
+		('inc', 'est'), ('israel'),
+		('jew', 'ish'), ('je', 'w'), ('je', 'ws'),
+		(' k', 'ill'), ('kk', 'k'),
+		('lol', 'i'),
+		('maste', 'r race'), ('mus', 'lim'),
+		('nation', 'alist'), ('na', 'zi'), ('nig', 'ga'), ('nig', 'ger'),
+		('pae', 'do'), ('pale', 'stin'), ('ped', 'o'),
+		('rac' 'ist'), (' r', 'ape'), ('ra', 'ping'),
+		('sl', 'ut'), ('swas', 'tika'),
+	]
+
 	_positive_keywords = []
-	_negative_keywords = []
+	_negative_keywords = ["".join(s) for s in _default_negative_keywords]
 
 	def __init__(self):
 		threading.Thread.__init__(self)
@@ -58,7 +78,9 @@ class RedditIO(threading.Thread, LogicMixin):
 		if self._config['DEFAULT']['positive_keywords']:
 			self._positive_keywords = self._config['DEFAULT']['positive_keywords'].lower().split(',')
 		if self._config['DEFAULT']['negative_keywords']:
-			self._negative_keywords = self._config['DEFAULT']['negative_keywords'].lower().split(',')
+			# Append user's custom negative keywords
+			self._negative_keywords += self._config['DEFAULT']['negative_keywords'].lower().split(',')
+
 		if self._config['DEFAULT']['subreddit']:
 			self._subreddit = self._config['DEFAULT']['subreddit'].strip()
 		else:
