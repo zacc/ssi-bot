8b35d58c4f0d4ca9c38df44a6f6587e591a6e656
zacc <zac.campbell@protonmail.com>
2021-12-11 13:18:12 +0800

    Fix error when checking the author_flair_text property of praw_thing (submissions/comments)

--- logic_mixin.py
+++ logic_mixin.py
@@ -160,9 +160,10 @@ class LogicMixin():
 
 		# Check the flair and username to see if the author might be a bot
 		# 'Verified GPT-2 Bot' is only valid on r/subsimgpt2interactive
-		if 'verified gpt-2 bot' in (praw_thing.author_flair_text or '').lower()\
+		# Sometimes author_flair_text will be present but None
+		if 'verified gpt-2' in (getattr(praw_thing, 'author_flair_text', '') or '').lower()\
 			or any(praw_thing.author.name.lower().endswith(i) for i in ['ssi', 'bot', 'gpt2']):
-
+			# Reduce the reply probability by 10% to prioritise replying to humans
 			base_probability += -0.1
 		else:
 			# assume humanoid if author metadata doesn't meet the criteria for a bot
