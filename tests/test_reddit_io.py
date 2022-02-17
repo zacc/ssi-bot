27fb092c02b707fcef6efda727f59f3054cfc54d
zacc <zac.campbell@protonmail.com>
2022-02-17 15:26:09 +0800

    fix io test

--- tests/test_reddit_io.py
+++ tests/test_reddit_io.py
@@ -7,20 +7,20 @@ from reddit_io.reddit_io import *
 class TestCanReplyToPrawThing():
 
 	@pytest.mark.parametrize("pickle_path, expected",
-		[('tests/pickles/comment_mod_removed.pkl', False),
-		('tests/pickles/comment_user_deleted.pkl', False),
-		('tests/pickles/submission_mod_removed.pkl', False),
-		('tests/pickles/submission_spam_removed.pkl', False),
-		('tests/pickles/submission_user_deleted.pkl', False),
-		('tests/pickles/submission_locked.pkl', False),
-		('tests/pickles/comment_submission_locked.pkl', False),
-		('tests/pickles/submission_selftext.pkl', True),
-		('tests/pickles/comment.pkl', True),
+		[('tests/pickles/comment_mod_removed.pkl', True),
+		('tests/pickles/comment_user_deleted.pkl', True),
+		('tests/pickles/submission_mod_removed.pkl', True),
+		('tests/pickles/submission_spam_removed.pkl', True),
+		('tests/pickles/submission_user_deleted.pkl', True),
+		('tests/pickles/submission_locked.pkl', True),
+		('tests/pickles/comment_submission_locked.pkl', True),
+		('tests/pickles/submission_selftext.pkl', False),
+		('tests/pickles/comment.pkl', False),
 		])
 	def test_can_reply_to_praw_thing(self, pickle_path, expected):
 
 		fh = open(pickle_path, 'rb')
 		thing = pickle.load(fh)
 
-		result = RedditIO._can_reply_to_praw_thing(RedditIO, thing)
+		result = RedditIO._is_praw_thing_removed_or_deleted(RedditIO, thing)
 		assert result == expected
