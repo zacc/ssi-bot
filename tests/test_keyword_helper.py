7d81b5b928de50f8b5d9b1b0bf2fc7a344108d87
zacc <zac.campbell@protonmail.com>
2022-03-15 14:24:39 +0800

    Keyword test not working properly.

--- tests/test_keyword_helper.py
+++ tests/test_keyword_helper.py
@@ -7,9 +7,9 @@ from utils.keyword_helper import KeywordHelper
 class TestKeywordHelper():
 
     @pytest.mark.parametrize("test_input, expected",
-        [('|>kill all bots', ['kill']),
-        ('the killer was spotted', ['kill']),
-        ('Killing in the name of', ['kill'])])
+        [('|>murder all bots', ['murder']),
+        ('the murder was spotted', ['murder']),
+        ('Murdering in the name of', ['murder'])])
     def test_negative_keyword_matching(self, test_input, expected):
         kh = KeywordHelper()
         matches = kh.negative_keyword_matches(test_input)
